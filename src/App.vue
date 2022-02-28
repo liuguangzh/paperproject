b96e80df644b8ad3fefbbcb845ac08ed401e53b5
project <757140163@qq.com>
2022-02-28 23:20:12 +0800

    论文项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+  </nav>
+  <router-view/>
+</template>
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    ddddd
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class HomeView extends Vue {}
+</script>
